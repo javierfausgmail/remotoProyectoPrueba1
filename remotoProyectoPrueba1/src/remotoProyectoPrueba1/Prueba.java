e80b8e0c60cd34dbe001315b8b2ab0834b9abadb
javie <javie@javi-x360>
2021-10-22 11:44:34 +0200

    Añadido el archivo requerido X

---
+++ remotoProyectoPrueba1/src/remotoProyectoPrueba1/Prueba.java
@@ -0,0 +1,16 @@
+package remotoProyectoPrueba1;
+
+public class Prueba {
+
+	public Prueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Esto es una prueba para GitHub");
+		
+	}
+
+}
