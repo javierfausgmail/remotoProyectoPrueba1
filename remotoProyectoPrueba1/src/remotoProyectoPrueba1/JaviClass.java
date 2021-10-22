96c2c3f19120f427ccef040c4218943fac8d5edf
javie <javie@javi-x360>
2021-10-22 12:43:01 +0200

    Primer commit a mi rama personal

--- remotoProyectoPrueba1/src/remotoProyectoPrueba1/JaviClass.java
+++ remotoProyectoPrueba1/src/remotoProyectoPrueba1/JaviClass.java
@@ -1,16 +1,15 @@
 package remotoProyectoPrueba1;
 
-public class Prueba {
+public class JaviClass {
 
-	public Prueba() {
+	public JaviClass() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Esto es una prueba para GitHub");
-		
+		System.out.println("Hola soy Javi");
 	}
 
 }
