057aa4440d2a4fd5bf1aef62afab7b57fdca7ab8
Nullgosling <Nullgosling@172.26.1.227>
2021-10-22 12:40:27 +0200

    Esto es un comentario

---
+++ remotoProyectoPrueba1/src/remotoProyectoPrueba1/Cristina.java
@@ -0,0 +1,14 @@
+package remotoProyectoPrueba1;
+
+public class Cristina {
+
+	public Cristina() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, soy Cristina");
+	}
+
+}
