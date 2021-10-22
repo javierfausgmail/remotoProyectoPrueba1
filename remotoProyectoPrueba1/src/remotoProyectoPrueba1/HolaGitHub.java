19da08463ed93a060420d4e7a0327658137dba9a
Win81 <Win81@PCVirtual>
2021-10-22 13:42:52 +0200

    Modificado el archivo

--- remotoProyectoPrueba1/src/remotoProyectoPrueba1/HolaGitHub.java
+++ remotoProyectoPrueba1/src/remotoProyectoPrueba1/HolaGitHub.java
@@ -3,7 +3,7 @@ package remotoProyectoPrueba1;
 public class HolaGitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Modificada la salida a pantalla
 		System.out.println("Hola soy JaviC7. ");
 	}
 
