5c2ae9201aec128005f45b8611465b3907cbd2ec
Inma <Inma@LAPTOP-J610EI8M>
2021-10-22 13:45:03 +0200

    creamos branch de Inma

---
+++ remotoProyectoPrueba1/src/remotoProyectoPrueba1/inmaClass.java
@@ -0,0 +1,10 @@
+package remotoProyectoPrueba1;
+
+public class inmaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola soy Inma");
+	}
+
+}
